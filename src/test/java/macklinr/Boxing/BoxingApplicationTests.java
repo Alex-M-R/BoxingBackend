2246cff75c100d258646d75ae214333380f753c7
AlexMR-Unity <chaosborne123@gmail.com>
2022-11-03 09:57:49 -0500

    Initial commit

---
+++ src/test/java/macklinr/Boxing/BoxingApplicationTests.java
@@ -0,0 +1,13 @@
+package macklinr.Boxing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoxingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
